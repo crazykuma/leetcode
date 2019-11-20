58dcc5d72dcfa35b92e0dcb26765e43bd866b2bd
crazykuma <crazykuma@qq.com>
2019-11-20 18:25:03 +0800

    最大连续1的个数

--- python3/0485.py
+++ python3/0485.py
@@ -1,27 +1,7 @@
 from typing import List
 
 class Solution:
-    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        i=0
-        maxCount=0
-        count=0
-        while i<len(nums):
-            if i!=len(nums)-1:
-                if nums[i]==1:
-                    count+=1
-                else:
-                    if count>maxCount:
-                        maxCount=count
-                    count=0
-            else:
-                if nums[i]==1:
-                    count+=1
-                if count>maxCount:
-                    maxCount=count
-            i+=1
-        return maxCount
-
-    def findMaxConsecutiveOnes2(self,nums:List[int])->int:
+    def findMaxConsecutiveOnes(self,nums:List[int])->int:
         count=0
         maxCount=0
         for i in range(len(nums)):
@@ -35,5 +15,5 @@ class Solution:
 
 if __name__ == "__main__":
     s=Solution()
-    print(s.findMaxConsecutiveOnes2([1]))
-    print(s.findMaxConsecutiveOnes2([1,1,0,1,1,1,0]))
+    print(s.findMaxConsecutiveOnes([1]))
+    print(s.findMaxConsecutiveOnes([1,1,0,1,1,1,0]))
