fc4c1ae4e0d2d6a848ec379a39666ddf4661ece5
crazykuma <crazykuma@qq.com>
2019-11-27 01:58:41 +0800

    斐波那契数

---
+++ python3/0509.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, N: int) -> int:
+        if N==0:
+            return 0
+        elif N==1:
+            return 1
+        else:
+            return self.fib(N-1)+self.fib(N-2)
+
+if __name__ == "__main__":
+    s=Solution()
+    print(s.fib(3))
+    print(s.fib(4))
