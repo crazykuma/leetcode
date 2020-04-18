e9d6778c443ec503844acf4a13680b58b5ec5709
crazykuma <crazykuma@qq.com>
2020-04-18 16:05:24 +0800

    二叉树的最大深度

--- python3/0104.py
+++ python3/0104.py
@@ -11,22 +11,19 @@ class Solution:
         # 递归，其实是后序遍历，求出左节点的值与右节点的值比较取大值+1作为树根节点的值
         if not root:
             return 0
-        ans = max(self.maxDepth1(root.left)+1,self.maxDepth1(root.right)+1)
+        ans = max(self.maxDepth1(root.left)+1, self.maxDepth1(root.right)+1)
         return ans
-            
+
     def maxDepth2(self, root: TreeNode) -> int:
-        # 前序遍历，记录下每个节点的深度，然后取最大
-        res=[0]
-        
-        def travel(root,depth):
+        # 前序遍历,先计算中间节点的值，再计算左右节点的值
+
+        def travel(root, depth):
             if not root:
-                return max(res)
-            depth+=1
-            res.append(depth)
-            if root.left:
-                travel(root.left,depth)
-            if root.right:
-                travel(root.right,depth)
-            
-        travel(root,0)
-        return max(res)
+                return depth
+            depth += 1
+            l = travel(root.left, depth) if root.left else depth
+            r = travel(root.right, depth) if root.right else depth
+
+            return max(l, r)
+
+        return travel(root, 0)
