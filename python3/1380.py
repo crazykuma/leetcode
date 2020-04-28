900bdf58743b8b75a3540b98a3910db67a68b912
crazykuma <crazykuma@qq.com>
2020-04-28 13:02:48 +0800

    fix:修正bug，增加测试用例

--- python3/1380.py
+++ python3/1380.py
@@ -6,7 +6,7 @@ class Solution:
         # 两种方法可以生成列，注意zip方法和*传参
         minrow = [min(i) for i in matrix]
         # maxcol = [max([arr[col] for arr in matrix])
-        #           for col in range(len(matrix))]
+        #           for col in range(len(matrix[0]))]
         maxcol = [max(i) for i in zip(*matrix)]
         return [i for i in minrow if i in maxcol]
 
@@ -14,3 +14,4 @@ class Solution:
 if __name__ == "__main__":
     s = Solution()
     print(s.luckyNumbers([[3, 7, 8], [9, 11, 13], [15, 16, 17]]))
+    print(s.luckyNumbers([[1, 10, 4, 2], [9, 3, 8, 7], [15, 16, 17, 12]]))
