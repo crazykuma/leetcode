2a368f971507bb7fc122b67784f2cb89171de141
crazykuma <crazykuma@qq.com>
2020-05-02 18:37:47 +0800

    两个数组的交集

--- python3/0349.py
+++ python3/0349.py
@@ -1,11 +1,16 @@
 from typing import List
 
+
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         return [i for i in set(nums1) if i in set(nums2)]
 
-if __name__ == "__main__":
-    s=Solution()
-    print(s.intersection([1,2,2,1],[2,2]))  # [2]
-    print(s.intersection([4,9,5],[9,4,9,8,4])) #[9,4]
+    def intersection2(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # 增加set集合方法
+        return list(set(nums1) & set(nums2))
 
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.intersection2([1, 2, 2, 1], [2, 2]))  # [2]
+    print(s.intersection2([4, 9, 5], [9, 4, 9, 8, 4]))  # [9,4]
