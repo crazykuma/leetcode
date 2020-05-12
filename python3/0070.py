2ceeaa9d52c648572972dd0f2321edbb4624dc95
crazykuma <crazykuma@qq.com>
2020-05-12 18:29:12 +0800

    爬楼梯：增加动态规划

--- python3/0070.py
+++ python3/0070.py
@@ -1,6 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         # 超出时间限制
+        # 其实跟斐波那契数列一模一样
+        # 1 1 2 3 5 8 13 .....
         if n == 1:
             return 1
         elif n == 2:
@@ -10,17 +12,30 @@ class Solution:
 
     def climbStairs2(self, n: int) -> int:
         # 36ms
-        a=1
-        b=2
-        if n==1:
+        a = 1
+        b = 2
+        if n == 1:
             return a
-        elif n==2:
+        elif n == 2:
             return b
         else:
-            for _ in range(2,n):
+            for _ in range(2, n):
                 # 1 2 3 5 8
-                a,b=b,a+b
+                a, b = b, a+b
             return b
+
+    def climbStairs3(self, n: int) -> int:
+        # 动态规划
+        # 第n个台阶只能从第n-1和第n-2个台阶上来
+        # 因此状态方程dp[n]=dp[n-1]+dp[n-2]
+        dp = [0]*(n+1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1]+dp[i-2]
+
+        return dp[n]
+
 
 if __name__ == "__main__":
     s = Solution()
